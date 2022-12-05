23909abb4e13c8ac8450faeffeedebd6445528c7
dindaputriay <dindaputri@mhs.mdp.ac.id>
2022-12-05 11:20:59 +0700

    1

--- app/src/main/java/com/if3b/quotes/API/APIRequestData.java
+++ app/src/main/java/com/if3b/quotes/API/APIRequestData.java
@@ -1,10 +1,14 @@
 package com.if3b.quotes.API;
 
+import com.if3b.quotes.Model.QuotesModel;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIRequestData {
     @GET("quotes")
-    Call
+    Call <List<QuotesModel>> requestData();
 
 }
